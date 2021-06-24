1a9942c93b784ca79535402c1598e566bb3879c1
MChris173 <mfonchris273@yahoo.com>
2021-06-24 19:31:51 +0100

    completed 48 - 55 Remove Tracks from a Playlist

--- jamming/src/Components/Track/Track.js
+++ jamming/src/Components/Track/Track.js
@@ -5,15 +5,18 @@ import './Track.css';
 class Track extends React.Component {
     constructor(props) {
         super(props);
+
         this.addTrack = this.addTrack.bind(this);
+        this.removeTrack = this.removeTrack.bind(this);
+        
     }
 
 renderAction() {
 
     if (this.props.isRemoval) {
-       return <button className = "Track-action">-</button>
+       return <button className="Track-action" onClick={this.removeTrack}>-</button>
     } else {
-        return <button className = "Track-action" onClick={this.addTrack}>+</button>
+        return <button className="Track-action" onClick={this.addTrack}>+</button>
     
     }
 }
@@ -22,6 +25,9 @@ renderAction() {
         this.props.onAdd(this.props.track);
     }
 
+    removeTrack() {
+        this.props.onRemove(this.props.track)
+    }
 
     render() {
         return(
